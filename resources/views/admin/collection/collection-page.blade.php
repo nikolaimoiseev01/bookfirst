4a3a66c168aef6437cc6eb5000d19b559afbec22
nikolaimoiseev01 <89159295+nikolaimoiseev01@users.noreply.github.com>
2022-02-20 21:43:49 +0300

    minor

--- resources/views/admin/collection/collection-page.blade.php
+++ resources/views/admin/collection/collection-page.blade.php
@@ -579,7 +579,7 @@
         <div class="col-md-12">
             <div class="card collapsed-card mt-0">
                 <div class="bg-gradient-cyan card-header">
-                    <h1 style="font-size: 25px;" class="card-title">Печатные экзепляры</h1>
+                    <h1 style="font-size: 25px;" class="card-title">Печатные экзепляры оплативших клиентов</h1>
 
                     <div class="card-tools">
                         <button class="btn btn-tool" data-card-widget="collapse">
