f13c9d1ede6cd76650b637a1a5139718601cf9af
nikolaimoiseev01 <89159295+nikolaimoiseev01@users.noreply.github.com>
2023-01-04 17:44:20 +0300

    minor

--- app/Http/Middleware/UserLastActivity.php
+++ app/Http/Middleware/UserLastActivity.php
@@ -19,13 +19,13 @@ class UserLastActivity
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()) {
-            $expireTime = Carbon::now()->addMinute(1); // keep online for 1 min
-            Cache::put('is_online'.Auth::user()->id, true, $expireTime);
-
-            //Last Seen
-            User::where('id', Auth::user()->id)->update(['last_seen' => Carbon::now()]);
-        }
-        return $next($request);
+//        if (Auth::check()) {
+//            $expireTime = Carbon::now()->addMinute(1); // keep online for 1 min
+//            Cache::put('is_online'.Auth::user()->id, true, $expireTime);
+//
+//            //Last Seen
+//            User::where('id', Auth::user()->id)->update(['last_seen' => Carbon::now()]);
+//        }
+//        return $next($request);
     }
 }
