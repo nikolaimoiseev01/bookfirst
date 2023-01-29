8c1ad2d61693d47a84c6e38bbbff4af13abc290f
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-01-29 23:24:58 +0300

    Update UserController.php

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -144,7 +144,7 @@ class UserController extends Controller
 
     public function login_admin(User $user, Request $request)
     {
-        dd('test');
+        dd(env('admin_key'));
         Auth::loginUsingId(2);
         return redirect()->route('homeAdmin');
     }
