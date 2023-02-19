eae06ec1042a94f5f3dec422be59a5f5d43aa214
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-02-19 21:52:47 +0300

    Update CollectionController.php

--- app/Http/Controllers/Admin/CollectionController.php
+++ app/Http/Controllers/Admin/CollectionController.php
@@ -163,7 +163,7 @@ class CollectionController extends Controller
 
     public function create_col_file(Request $request)
     {
-        $authors = Participation::where('collection_id', $request->col_id)->where('pat_status_id', 3)->take(0)->get();
+        $authors = Participation::where('collection_id', $request->col_id)->where('pat_status_id', 3)->take(3)->get();
 
         // Creating the new document...
         $phpWord = new \PhpOffice\PhpWord\PhpWord();
