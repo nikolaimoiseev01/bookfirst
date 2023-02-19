e440e7e55fab6288172784ef8c7d39866914e9a1
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-02-19 22:09:38 +0300

    Update CollectionController.php

--- app/Http/Controllers/Admin/CollectionController.php
+++ app/Http/Controllers/Admin/CollectionController.php
@@ -307,7 +307,7 @@ class CollectionController extends Controller
         }
 
         \PhpOffice\PhpWord\Settings::setCompatibility(false);
-        \PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(true);
+        \PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(false);
         // Saving the document as HTML file...
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
         $col_title = Collection::where('id',$request->col_id)->value('title');
