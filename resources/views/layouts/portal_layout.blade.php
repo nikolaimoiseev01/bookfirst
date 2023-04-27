81ac5ee1fa9ba960b45737c0079b575a73e237d0
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-04-27 13:16:34 +0300

    Update portal_layout.blade.php

--- resources/views/layouts/portal_layout.blade.php
+++ resources/views/layouts/portal_layout.blade.php
@@ -255,22 +255,22 @@
 
 {{--Новогодние снежинки--}}
 
-<script src="https://unpkg.com/magic-snowflakes/dist/snowflakes.min.js"></script>
-<script>
-    new Snowflakes({
-        color: '#5ECDEF', // Default: "#5ECDEF"
-        container: document.body, // Default: document.body
-        count: 20, // 100 snowflakes. Default: 50
-        minOpacity: 0.4, // From 0 to 1. Default: 0.6
-        maxOpacity: 0.8, // From 0 to 1. Default: 1
-        minSize: 10, // Default: 10
-        maxSize: 20, // Default: 25
-        rotation: true, // Default: true
-        speed: 1, // The property affects the speed of falling. Default: 1
-        wind: true, // Without wind. Default: true
-        zIndex: 9997 // Default: 9999
-    });
-</script>
+{{--<script src="https://unpkg.com/magic-snowflakes/dist/snowflakes.min.js"></script>--}}
+{{--<script>--}}
+{{--    new Snowflakes({--}}
+{{--        color: '#5ECDEF', // Default: "#5ECDEF"--}}
+{{--        container: document.body, // Default: document.body--}}
+{{--        count: 20, // 100 snowflakes. Default: 50--}}
+{{--        minOpacity: 0.4, // From 0 to 1. Default: 0.6--}}
+{{--        maxOpacity: 0.8, // From 0 to 1. Default: 1--}}
+{{--        minSize: 10, // Default: 10--}}
+{{--        maxSize: 20, // Default: 25--}}
+{{--        rotation: true, // Default: true--}}
+{{--        speed: 1, // The property affects the speed of falling. Default: 1--}}
+{{--        wind: true, // Without wind. Default: true--}}
+{{--        zIndex: 9997 // Default: 9999--}}
+{{--    });--}}
+{{--</script>--}}
 {{-------------------------------------------------------------------------}}
 
 
