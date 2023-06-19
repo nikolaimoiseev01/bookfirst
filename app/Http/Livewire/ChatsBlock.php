43ffbe289b44886f9e1c58a8a8af45d7ff57514f
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-06-20 00:11:32 +0300

    Update ChatsBlock.php

--- app/Http/Livewire/ChatsBlock.php
+++ app/Http/Livewire/ChatsBlock.php
@@ -149,11 +149,11 @@ class ChatsBlock extends Component
 
 
         } else {
-            if ($this->user_chats ?? null) {
-                $this->chosen_chat_id = $this->user_chats[0]->id; // видим id последнего чата
-            } else {
-                $this->chosen_chat_id = 0; // видим id последнего чата
-            }
+//            if ($this->user_chats ?? null) {
+//                $this->chosen_chat_id = $this->user_chats[0]->id; // видим id последнего чата
+//            } else {
+//                $this->chosen_chat_id = 0; // видим id последнего чата
+//            }
 
         }
 
