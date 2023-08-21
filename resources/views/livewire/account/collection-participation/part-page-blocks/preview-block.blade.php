637949381b3d9f6bab4841eadb81943ce80c2b3b
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-08-21 19:23:43 +0300

    Update preview-block.blade.php

--- resources/views/livewire/account/collection-participation/part-page-blocks/preview-block.blade.php
+++ resources/views/livewire/account/collection-participation/part-page-blocks/preview-block.blade.php
@@ -8,9 +8,12 @@
             <h2>{{$page_title}}</h2>
         </div>
         <div class="info_wrap">
-
+            {{$collection['col_status_id']}}
+            {{$participation['pat_status_id']}}
 
             @if ($collection['col_status_id'] >= 2 && ($participation['pat_status_id'] === 1 || $participation['pat_status_id'] === 9))
+
+
                 <p class="no-access">Из-за отсутствия оплаты Вы не были включены в список участников.</p>
             @elseif($collection['col_status_id'] === 1)
 
