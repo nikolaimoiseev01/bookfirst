ed1ab994aff5bf51b04f72fdd0dd9294dbc227a4
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-11-19 17:14:00 +0300

    Update AdminSocialController.php

--- app/Http/Controllers/Admin/AdminSocialController.php
+++ app/Http/Controllers/Admin/AdminSocialController.php
@@ -124,6 +124,8 @@ WHERE dt.datetime between '2022-12-10' and sysdate()";
 
         $data_works_uploaded = collect(DB::raw($query_works_uploaded));
 
+        dd($data_works_uploaded);
+
         $query_new_users = "select DATE_FORMAT(dt.datetime, '%d.%m') AS date,  cnt_users from dc_date dt
 
                 left join (
