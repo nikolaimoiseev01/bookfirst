ce30367d4ff26b515f1b16bfee95c3a7ec079dc8
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-12-11 21:42:12 +0300

    Update js.js

--- public/js/js.js
+++ public/js/js.js
@@ -262,20 +262,19 @@ function trigger_all_js() {
 //endregion
 
 //region -- Новогодние снежинки
-
-    new Snowflakes({
-        color: '#5ECDEF', // Default: "#5ECDEF"
-        container: document.body, // Default: document.body
-        count: 20, // 100 snowflakes. Default: 50
-        minOpacity: 0.4, // From 0 to 1. Default: 0.6
-        maxOpacity: 0.8, // From 0 to 1. Default: 1
-        minSize: 10, // Default: 10
-        maxSize: 20, // Default: 25
-        rotation: true, // Default: true
-        speed: 1, // The property affects the speed of falling. Default: 1
-        wind: true, // Without wind. Default: true
-        zIndex: 9997 // Default: 9999
-    });
+new Snowflake1s({
+    color: '#5ECDEF', // Default: "#5ECDEF"
+    container: document.body, // Default: document.body
+    count: 20, // 100 snowflakes. Default: 50
+    minOpacity: 0.4, // From 0 to 1. Default: 0.6
+    maxOpacity: 0.8, // From 0 to 1. Default: 1
+    minSize: 10, // Default: 10
+    maxSize: 20, // Default: 25
+    rotation: true, // Default: true
+    speed: 1, // The property affects the speed of falling. Default: 1
+    wind: true, // Without wind. Default: true
+    zIndex: 9997 // Default: 9999
+});
 //endregion
 
 
