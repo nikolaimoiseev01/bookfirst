a6e833ee4ef96f2a26fd86ce2fe8863da130b15b
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2023-12-22 13:55:35 +0300

    minor

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\own_book;
+use Exception;
 use Illuminate\Console\Command;
 use setasign\Fpdi\Fpdi;
 
@@ -32,36 +33,50 @@ class TestCommand extends Command
 
         foreach ($own_books as $own_book) {
 
+            try {
             // Создаем обрезанную версию ВБ
 
-            if ($own_book['inside_file'] and file_exists($own_book['inside_file'])) {
+            $inside_file_path = public_path('/' . $own_book['inside_file']);
+            $file_exist = file_exists($inside_file_path);
+
+            if ($own_book['inside_file'] and $file_exist) {
+
+
                 $pdfPath = $own_book['inside_file'];
                 $user_folder = 'admin_files/own_books/' . 'user_id_' . $own_book['user_id'] . '/' . $own_book['title'] . '/ВЕРСТКА/';
-                $cut_file_path = $user_folder . 'ВБ_Main_' . $own_book['title'] . '_CUT.pdf';
+                $cut_file_path = public_path($user_folder . 'ВБ_Main_' . $own_book['title'] . '_CUT.pdf');
 
                 // Понимаем размер файла
                 $pdf = new Fpdi();
-                $pageCount = $pdf->setSourceFile($pdfPath);
+                $pageCount = $pdf->setSourceFile($inside_file_path);
                 $templateId = $pdf->importPage(1);
                 $size = $pdf->getTemplateSize($templateId);
 
-                // Создайте экземпляр Fpdi
+//                 Создайте экземпляр Fpdi
                 $pdf = new Fpdi('P', 'mm', array(round($size['height']), round($size['width'])));
 
                 // Добавьте первые 10 страниц в новый PDF-документ
                 for ($page = 1; $page <= 10; $page++) {
                     $pdf->AddPage();
-                    $pdf->setSourceFile($pdfPath);
+                    $pdf->setSourceFile($inside_file_path);
                     $template = $pdf->importPage($page);
                     $size = $pdf->getTemplateSize($template);
                     $pdf->useTemplate($template);
                 }
 
-                $pdf->output($cut_file_path, 'F');
+                $pdf->output($cut_file_path,'F');
+
 
                 own_book::where('id', $own_book['id'])->update(array(
                     'inside_file_cut' => $cut_file_path,
                 ));
+            }
+
+            } catch (Exception $e) {
+                // обработка ошибки
+                echo 'Ошибка: ' . $e->getMessage() . '<br>';
+                // или просто продолжить цикл без прерывания
+                continue;
             }
         }
     }
