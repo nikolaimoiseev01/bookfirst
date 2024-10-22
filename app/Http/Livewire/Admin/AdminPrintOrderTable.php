c06a2595579aa996517952406b25f65685f59630
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2024-10-22 13:49:05 +0300

    Update AdminPrintOrderTable.php

--- app/Http/Livewire/Admin/AdminPrintOrderTable.php
+++ app/Http/Livewire/Admin/AdminPrintOrderTable.php
@@ -4,17 +4,18 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Participation;
 use App\Models\Printorder;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class AdminPrintOrderTable extends Component
 {
-    Public $track_number = [];
-    Public $send_price = [];
-    Public $participations;
-    Public $print_order_id = 99999;
-    Public $collection_id;
-    Public $show_input = 0;
-    Public $show_input_send = 0;
+    public $track_number = [];
+    public $send_price = [];
+    public $participations;
+    public $print_order_id = 99999;
+    public $collection_id;
+    public $show_input = 0;
+    public $show_input_send = 0;
 
 
     protected $listeners = [
@@ -24,7 +25,15 @@ class AdminPrintOrderTable extends Component
 
     public function render()
     {
-        $this->participations = Participation::join('printorders', 'participations.printorder_id', '=', 'printorders.id')
+        $this->participations = Participation::select(
+            'books_needed',
+            'participations.id as id',
+            'name',
+            'send_to_name',
+            'printorder_id',
+            'send_to_tel',
+            'books_needed')
+            ->join('printorders', 'participations.printorder_id', '=', 'printorders.id')
             ->where('participations.collection_id', $this->collection_id)
             ->where('participations.pat_status_id', 3)
             ->orderBy('books_needed')->get();
@@ -39,8 +48,7 @@ class AdminPrintOrderTable extends Component
 
 
         $this->collection_id = $collection_id;
-        foreach (Printorder::where('collection_id', $this->collection_id)->get()->toArray() as $print_order)
-        {
+        foreach (Printorder::where('collection_id', $this->collection_id)->get()->toArray() as $print_order) {
             $this->track_number[$print_order['id']] = $print_order['track_number'];
         }
 
@@ -58,15 +66,13 @@ class AdminPrintOrderTable extends Component
     public function save_track_number($print_order_id)
     {
 
-        if($this->track_number[$print_order_id] === '' || $this->track_number[$print_order_id] === null)
-        {
-           $this->dispatchBrowserEvent('swal:modal', [
+        if ($this->track_number[$print_order_id] === '' || $this->track_number[$print_order_id] === null) {
+            $this->dispatchBrowserEvent('swal:modal', [
                 'type' => 'error',
                 'title' => 'Что-то пошло не так',
                 'text' => 'Ни одно поле не должно быть пустым!',
-            ]);}
-        else
-        {
+            ]);
+        } else {
             // ---- Редактируем Заказ печатных! ---- //
             Printorder::where('id', $print_order_id)->update([
                 'track_number' => $this->track_number[$print_order_id],
@@ -81,16 +87,13 @@ class AdminPrintOrderTable extends Component
     public function save_send_price($print_order_id)
     {
 
-        if($this->send_price[$print_order_id] === '' || $this->send_price[$print_order_id] === null)
-        {
+        if ($this->send_price[$print_order_id] === '' || $this->send_price[$print_order_id] === null) {
             $this->dispatchBrowserEvent('swal:modal', [
                 'type' => 'error',
                 'title' => 'Что-то пошло не так',
                 'text' => 'Ни одно поле не должно быть пустым!',
             ]);
-        }
-        else
-        {
+        } else {
             // ---- Редактируем Заказ печатных! ---- //
             Printorder::where('id', $print_order_id)->update([
                 'send_price' => $this->send_price[$print_order_id],
