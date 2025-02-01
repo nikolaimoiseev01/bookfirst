0153f350fc5276336d9f01066cf6bccdfc6cb881
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2025-02-01 14:32:18 +0300

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            return (new MailMessage)
+                ->subject(Lang::get('Восстановление пароля'))
+                ->greeting('Здравствуйте!')
+                ->line(Lang::get('Вы получили это письмо, поскольку мы получили запрос на сброс пароля для вашей учетной записи.'))
+                ->action(Lang::get('Сбросить пароль'), route('password.reset', $token))
+                ->line(Lang::get('Ссылка перестанет работать через минут: :count.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+                ->line(Lang::get('Если вы не запрашивали сброс пароля, никаких дальнейших действий не требуется.'))
+                ->salutation('С уважением, ' . config('app.name'))
+                ;
+        });
     }
 }
