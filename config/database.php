11f59794c4e0ae3c2421c4943453e99dd0d5ef08
Nikolai <nikolaimoiseev01@gmail.com>
2025-11-22 17:51:42 +0300

    minor update

--- config/database.php
+++ config/database.php
@@ -68,8 +68,8 @@ return [
             'database' => env('OLD_DB_DATABASE', 'old_db'),
             'username' => env('OLD_DB_USERNAME', 'root'),
             'password' => env('OLD_DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'cp1251',
+            'collation' => 'cp1251_general_ci',
         ],
 
         'mariadb' => [
