1b2afbe000b66f112e5c2941442d84305adc20c4
Nikolai <nikolaimoiseev01@gmail.com>
2025-11-22 17:42:28 +0300

    minor update

---
+++ app/Console/Commands/testUni.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class testUni extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-uni';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $text = DB::connection('old_mysql')->table('work_comments')->get()[0];
+        dd($text->text);
+    }
+}
