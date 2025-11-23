a993b55c24cce9c35a3a6b4eaa8f096d6844b090
Nikolai <nikolaimoiseev01@gmail.com>
2025-11-23 12:23:19 +0300

    minor update

--- app/Livewire/Pages/Account/OwnBook/OwnBooksPage.php
+++ app/Livewire/Pages/Account/OwnBook/OwnBooksPage.php
@@ -13,7 +13,7 @@ class OwnBooksPage extends Component
     public function render()
     {
         return view('livewire.pages.account.own-book.own-books-page', [
-            'own_books' => Auth::user()->ownBooks()->with(['media', 'ownBookStatus', 'ownBookInsideStatus', 'ownBookCoverStatus'])->simplePaginate(4)
+            'own_books' => Auth::user()->ownBooks()->with(['media'])->simplePaginate(4)
         ])->layout('layouts.account');
     }
 }
