29ff988b879e5922bc673a80aa3f117722f8cddc
Nikolai <nikolaimoiseev01@gmail.com>
2025-12-12 18:43:14 +0300

    New Year Theme

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -9,6 +9,7 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
@@ -17,30 +18,37 @@ class SocialiteController extends Controller
 {
     public function callback($provider)
     {
-        [$name, $surname] = match ($provider) {
-            'vkontakte' => ['first_name', 'last_name'],
-            'google'    => ['given_name', 'family_name'],
-            'yandex' => ['first_name', 'last_name'],
-            default     => [null, null],
-        };
+        return DB::transaction(function () use ($provider) {
 
-            $user = Socialite::driver($provider)->stateless()->user();
+            [$name, $surname] = match ($provider) {
+                'vkontakte' => ['first_name', 'last_name'],
+                'google' => ['given_name', 'family_name'],
+                'yandex' => ['first_name', 'last_name'],
+                default => [null, null],
+            };
 
+            $socialUser = Socialite::driver($provider)->stateless()->user();
             $user = User::firstOrCreate([
-                'email' => $user->email,
+                'email' => $socialUser->user['email'],
             ], [
-                'email' => $user->email,
-                'name' => $name,
-                'surname' => $surname,
+                'email' => $socialUser->user['email'],
+                'name' => $socialUser->user[$name],
+                'surname' => $socialUser->user[$surname],
                 'reg_type' => $provider,
                 'password' => Hash::make(Str::random(24)),
-                'email_verified_at' => Carbon::now()->toDateTimeString(),
-                'avatar' => $user->avatar
+                'email_verified_at' => Carbon::now()->toDateTimeString()
             ]);
+            if ($socialUser->avatar && $user->wasRecentlyCreated) {
+                try {
+                    $user->addMediaFromUrl($socialUser->avatar)
+                        ->toMediaCollection('avatar');
+                } catch (\Throwable $e) {
+                }
+            }
 
             $user->assignRole('user');
 
-            $alertText = match($user->wasRecentlyCreated) {
+            $alertText = match ($user->wasRecentlyCreated) {
                 true => "Вы успешно создали аккаунт через {$provider}! Для учетной записи (email: {$user->email}) был сгенерирован случайный пароль. Если входить через {$provider}, он не нужен. Но для возможности входа через email его необходимо сменить в настройках аккаунта.",
                 false => "Вы успешно вошли через {$provider}! У вас уже был аккаунт на нашем сайте. В него теперь можно входить по email или через {$provider}."
             };
@@ -52,5 +60,6 @@ class SocialiteController extends Controller
 
             Auth::loginUsingId($user['id']);
             return redirect()->route('account.participations');
-        }
+        });
+    }
 }
