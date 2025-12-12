fa129e0b349ac63c7e7395d57d4757fd31cc055c
Nikolai <nikolaimoiseev01@gmail.com>
2025-12-12 19:06:11 +0300

    minor update

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -4,9 +4,9 @@
 
 @php
     $classes = match($color) {
-        'white' => 'block text-white border text-xl border-white min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-white hover:text-green-500',
-        'green' => 'block text-green-500 border text-xl border-green-500 min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-green-500 hover:text-white',
-        'yellow' => 'block text-brown-400 border text-xl border-brown-400 min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-brown-400 hover:text-white'
+        'white' => 'block text-white border text-xl border-white min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-white hover:text-green-500 active:bg-white active:text-green-500',
+        'green' => 'block text-green-500 border text-xl border-green-500 min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-green-500 hover:text-white active:bg-green-500 active:text-white',
+        'yellow' => 'block text-brown-400 border text-xl border-brown-400 min-w-max flex gap-2 items-center justify-center rounded-lg py-1 px-8 cursor-pointer transition hover:bg-brown-400 hover:text-white active:bg-brown-400 active:text-white'
     };
 @endphp
 
