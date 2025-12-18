bcbdea411e3a5fb7d1e6dc0c249e9b306b0698ac
Nikolai <nikolaimoiseev01@gmail.com>
2025-12-18 10:29:22 +0300

    minor update

--- routes/console.php
+++ routes/console.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 
 Schedule::command('app:internal-tasks-notification')->dailyAt('19:30');
 Schedule::command('app:ext-promotion-stat-update')->dailyAt('21:00');
-Schedule::command('app:ext-promotion-stat-update')->dailyAt('21:15');
+Schedule::command('app:ext-promotion-finish')->dailyAt('21:15');
 
