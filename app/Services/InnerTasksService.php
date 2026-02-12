98a470c86dbbcf89d6f0b2b5b585a42ef0177977
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-12 23:03:46 +0300

    Minor

--- app/Services/InnerTasksService.php
+++ app/Services/InnerTasksService.php
@@ -78,6 +78,11 @@ class InnerTasksService
 
     private function handleGeneralStatus($ownBook)
     {
+        if($ownBook['paid_at_print_only'] == null &&
+            ($ownBook['status_general'] == OwnBookStatusEnums::PRINT_WAITING )
+        ) {
+            dd($ownBook);
+        }
         return match ($ownBook['status_general']) {
             OwnBookStatusEnums::REVIEW =>
             ["Принять заявку",
