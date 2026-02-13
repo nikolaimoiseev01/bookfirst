15a65ead72af78095fde6a18122c5e4d40378758
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-14 00:50:43 +0300

    Friend Invite System

--- resources/views/components/ui/news-toast.blade.php
+++ resources/views/components/ui/news-toast.blade.php
@@ -3,7 +3,7 @@
     // ключ в sessionStorage
     'storageKey' => 'referral_toast_closed_v1',
     // задержка перед показом (мс)
-    'delay' => 3000,
+    'delay' => 2000,
     'buttonText' => 'Подробнее'
 ])
 
