0baafaebcf627d3cacc869954b5536740d90a00a
nikolaimoiseev01 <89159295+nikolaimoiseev01@users.noreply.github.com>
2026-02-18 23:17:26 +0300

    ACA Commit

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+echo "🚀 Starting deployment..."
+
+# Переходим в директорию проекта
+cd /var/www/pervajakniga || exit
+
+# Забираем изменения
+git pull
+
+# Разрешаем composer работать под root (если всё же запускаешь под root)
+export COMPOSER_ALLOW_SUPERUSER=1
+
+# Установка зависимостей
+composer install
+
+php artisan optimize:clear
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan filament:optimize-clear
+php artisan filament:optimize
+
+# Миграции
+php artisan migrate
+
+# Перезапуск очереди (supervisor)
+supervisorctl restart laravel-worker:*
+
+echo "✨ Deployment finished!"
