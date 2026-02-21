2ccb7f1e9b922751517f583aec74541dca334fb2
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-21 21:06:29 +0300

    minor

--- app/Services/CdekPrintService.php
+++ app/Services/CdekPrintService.php
@@ -81,9 +81,9 @@ class CdekPrintService
             $sheet->setCellValue('A' . $key + 2, $cdek_desc); // Номер отправления
             $sheet->setCellValue('B' . $key + 2, $parsedAddressData['city'] ?? ''); // Город получателя
             $sheet->setCellValue('C' . $key + 2, $parsedAddressData['postal_code'] ?? ''); // Индекс города получателя
-            $sheet->setCellValue('D' . $key + 2, $printOrder['address_json']['receiver_name']); // Получатель
+            $sheet->setCellValue('D' . $key + 2, $printOrder['receiver_name']); // Получатель
             $sheet->setCellValue('E' . $key + 2, $printOrder['receiver_name']); // ФИО Получателя
-            $sheet->setCellValue('F' . $key + 2, $printOrder['string']); // Адрес получателя
+            $sheet->setCellValue('F' . $key + 2, $printOrder['address_json']['string']); // Адрес получателя
             $sheet->setCellValue('G' . $key + 2, $parsedAddressData['code'] ?? ''); // КОД ПВЗ
             $sheet->setCellValue('H' . $key + 2, $printOrder['receiver_telephone']); // Телефон получателя
             $sheet->setCellValue('I' . $key + 2, 1); // Доп сбор за доставку с получателя в т.ч. НДС
