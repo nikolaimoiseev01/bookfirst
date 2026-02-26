d54ca45e4df045d9762fe0cd574f802cd8b3830a
Nikolai <nikolaimoiseev01@gmail.com>
2026-02-26 19:01:35 +0300

    minor

--- app/Filament/Resources/Collection/Collections/Pages/EditCollection.php
+++ app/Filament/Resources/Collection/Collections/Pages/EditCollection.php
@@ -84,8 +84,8 @@ class EditCollection extends EditRecord
                     // Подгружаем участия сразу с медиа, чтобы не ловить N+1
                     $ownBooks = OwnBook::query()->where('own_books.status_general', OwnBookStatusEnums::DONE)
                         ->with('media')
-                        ->where('id', '>', 200)
-                        ->where('id', '<', 301)
+                        ->where('id', '>', 100)
+                        ->where('id', '<', 201)
                         ->get();
 
                     $filesAdded = 0;
