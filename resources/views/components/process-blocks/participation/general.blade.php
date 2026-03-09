cd5ffba0abb4411b6f7763f5d2ad0625a3d197ce
Nikolai <nikolaimoiseev01@gmail.com>
2026-03-09 13:09:13 +0300

    PurchasePrint System

--- resources/views/components/process-blocks/participation/general.blade.php
+++ resources/views/components/process-blocks/participation/general.blade.php
@@ -3,10 +3,13 @@
         <div class="flex justify-between gap-4 md:flex-col">
             <div class="flex flex-col w-1/2 md:w-full">
                 <p><b>Имя в сборнике: </b>{{$participation['author_name']}}</p>
-                <p><b>Проверка: </b>{{$participation['price_check'] > 0 ? 'требуется' : 'не требуется'}}</p>
+                <p>
+                    <b>Проверка: </b>{{$participation['price_check'] > 0 ? 'требуется' : 'не требуется'}}
+                </p>
                 <div class="flex gap-4 items-center">
                     <p><b>Произведений: </b>{{count($participation->participationWorks)}}</p>
-                    <span class="text-green-400 text-xl cursor-pointer" x-text="showWorks ? 'Свернуть' : 'Показать'"
+                    <span class="text-green-400 text-xl cursor-pointer"
+                          x-text="showWorks ? 'Свернуть' : 'Показать'"
                           @click="showWorks = !showWorks"></span>
                 </div>
                 <p>Строчек: {{$participation['rows']}}, Страниц: {{$participation['pages']}}</p>
@@ -16,8 +19,11 @@
                     <p class="font-semibold text-2xl">Печатные экземпляры</p>
                     <p><b>Количество: </b>{{$participation->printOrder['books_cnt']}} экз.</p>
                     <p><b>Фио получателя: </b>{{$participation->printOrder['receiver_name']}}</p>
-                    <p><b>Телефон получателя: </b>{{$participation->printOrder['receiver_telephone']}}</p>
-                    <p><b>Адрес получателя: </b>{{$participation->printOrder['address_json']['string']}}</p>
+                    <p><b>Телефон
+                            получателя: </b>{{$participation->printOrder['receiver_telephone']}}</p>
+                    <p><b>Адрес
+                            получателя: </b>{{$participation->printOrder['address_json']['string']}}
+                    </p>
                 @else
                     <p class="font-semibold text-2xl">Печатные экземпляры не требуются</p>
                 @endif
@@ -28,14 +34,18 @@
                 <p class="font-semibold text-2xl mb-2">Произведения к участию</p>
                 <div class="flex gap-4 flex-wrap ">
                     @foreach($participation->participationWorks as $work)
-                        <a href="{{route('social.work', $work->work['id'])}}"
-                           class="px-4 py-2 border rounded border-dark-100">{{$work->work['title']}}</a>
+                        @if($work->work ?? null)
+                            <a href="{{route('social.work', $work->work['id'])}}"
+                               class="px-4 py-2 border rounded border-dark-100">{{$work->work['title']}}</a>
+                        @endif
                     @endforeach
                 </div>
             </div>
         </div>
         @if($collection['status']->order() < 4)
-            <x-ui.link href="{{route('account.participation.edit', $participation['id'])}}">Изменить заявку</x-ui.link>
+            <x-ui.link href="{{route('account.participation.edit', $participation['id'])}}">Изменить
+                заявку
+            </x-ui.link>
         @endif
     </div>
 
