29bc3c15d6e1a8c516875431f7ee9b1e218bae33
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2026-07-26 11:19:53 +0300

    Email Marketing

--- resources/views/components/ui/cards/card-collection-wide.blade.php
+++ resources/views/components/ui/cards/card-collection-wide.blade.php
@@ -5,17 +5,24 @@
     <div class="flex flex-col gap-4 lg:items-center lg:text-center">
         <h3>{{$collection['title']}}</h3>
         <p>{{$collection['description']}}</p>
+        @if($collection->workType ?? null)
+            <p class="mt-auto"><b>Тип произведений: {{$collection->workType->name}}</b></p>
+        @endif
+
     </div>
     <div class="flex flex-col justify-center gap-4 lg:w-full">
         <x-ui.link href="{{route('portal.collection', ['slug' => $collection['slug']])}}">Подробнее</x-ui.link>
-        <x-ui.link data-check-logged href="{{route('account.participation.create', ['collection_id' => $collection['id']])}}">Принять участие</x-ui.link>
+        <x-ui.link data-check-logged
+                   href="{{route('account.participation.create', ['collection_id' => $collection['id']])}}">Принять
+            участие
+        </x-ui.link>
     </div>
     <div class="absolute top-0 right-8 inline-block md:right-0 md:left-1/2 md:-translate-x-1/2 w-fit">
         <div
             class="bg-green-500 text-white text-center px-4 pb-4 text-lg flex flex-col
            [clip-path:polygon(0_0,100%_0,100%_100%,50%_85%,0_100%)]">
-                    <span>Заявки до:</span>
-                    <span>{{ \Carbon\Carbon::parse($collection['date_apps_end'])->translatedFormat('j F') }}</span>
+            <span>Заявки до:</span>
+            <span>{{ \Carbon\Carbon::parse($collection['date_apps_end'])->translatedFormat('j F') }}</span>
         </div>
     </div>
 </div>
