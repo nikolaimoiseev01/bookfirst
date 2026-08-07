7e32114c9f26a297f0b43a6ea9fb44a1380c5ed1
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2026-08-07 20:05:59 +0300

    Email Marketing

--- database/migrations/2026_08_07_000000_add_unique_index_to_email_campaign_recipients_table.php
+++ database/migrations/2026_08_07_000000_add_unique_index_to_email_campaign_recipients_table.php
@@ -9,6 +9,14 @@ return new class extends Migration
 {
     public function up(): void
     {
+        DB::statement('
+            DELETE t1 FROM email_campaign_recipients t1
+            INNER JOIN email_campaign_recipients t2
+                ON t1.email_campaign_id = t2.email_campaign_id
+                AND t1.email_recipient_id = t2.email_recipient_id
+                AND t1.id > t2.id
+        ');
+
         Schema::table('email_campaign_recipients', function (Blueprint $table) {
             $table->unique(['email_campaign_id', 'email_recipient_id'], 'campaign_recipient_unique');
         });
