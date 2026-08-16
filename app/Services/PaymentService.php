b8a16a7c52f44ccdb94b052b78c71b8d3fb659d1
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2026-08-16 22:05:13 +0300

    Robokassa

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -126,7 +126,7 @@ class PaymentService
                 'SignatureValue' => $signature,
             ];
 
-            dd($params);
+            dd($params, $password1);
 
             if ($isTest) {
                 $params['IsTest'] = 1;
